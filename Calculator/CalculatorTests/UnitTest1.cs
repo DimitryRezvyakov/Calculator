ab81dde69bba03e8e4136f3aa9b0aac9cada3cfb
DimitryRezvyakov <dimrez199@gmail.com>
2025-03-22 21:17:53 +0300

    Update UnitTest1.cs
    Все тесты проходят, есть случай деления на 0

--- Calculator/CalculatorTests/UnitTest1.cs
+++ Calculator/CalculatorTests/UnitTest1.cs
@@ -1,4 +1,3 @@
-
 using HomeWork;
 
 namespace CalculatorTests
@@ -16,6 +15,14 @@ namespace CalculatorTests
             Assert.Equal(expected, result);
         }
 
+        [Fact]
+        public void QuotienCalculateTest_OnZeroDivision_ReturnExeption()
+        {
+            var calc = new Calculator(1, 0, new QuotienCalculate());
+
+            Assert.Throws<DivideByZeroException>(() => calc.Calculate());
+        }
+
         public static IEnumerable<object[]> OperationsTestsData()
         {
             yield return new object[] {1, 2, 3, new SumCalculate() };
@@ -24,6 +31,8 @@ namespace CalculatorTests
             yield return new object[] { 6, 2, 3, new QuotienCalculate() };
         }
     }
+
+
 
     public class ParserTest
     {
@@ -40,8 +49,8 @@ namespace CalculatorTests
         public static IEnumerable<object[]> ParserData()
         {
             yield return new object[] { "1+ 2/ 3 -3*4-999", new string[] {"1", "+", "2", "/", "3", "-", "3", "*", "4", "-", "999" } };
-            yield return new object[] { "--++", new string[] { "-", "-", "+", "+"} };
-            yield return new object[] { "", new string[0]  };
+            yield return new object[] { "-1 -4", new string[] { "-1", "-", "4"} };
+            yield return new object[] { "1 +9*-5/-5-5-5", new string[] {"1", "+", "9", "*", "-5", "/", "-5", "-", "5", "-", "5" }  };
         }
     }
 
@@ -55,6 +64,7 @@ namespace CalculatorTests
             var arr = Parser.Parse(input);
             var nums = new List<double>();
             var opers = new List<string>();
+            Programm.DivideArr(nums, opers, arr);
             var runner = new MultiCalculator(opers, nums);
 
             var result = runner.Runner(0);
@@ -65,7 +75,8 @@ namespace CalculatorTests
         public static IEnumerable<object[]> RunnerData()
         {
             yield return new object[] { "400 / 20 + 50 * 20 / 5 + 100 - 500 * 100", -49680};
-            yield return new object[] { "55+20 / 2 +50 * 4-50 *2/2", 225 };
+            yield return new object[] { "55+20 / 2 +50 * 4-50 *2/2", 215 };
+            yield return new object[] { "5 - 3 + -10 * 2 / 5", -2 };
         }
     }
 }
